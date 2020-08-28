bcf493c3c4ec1e9673018fb8c3af4f30ee1c4053
Dandoleiro <Dandoleiro@users.noreply.github.com>
2020-08-28 17:18:26 +0200

    Menù
    Creazione menù di gioco e menù di pausa

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+  
+   public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+   public void Pause()
+    {
+        Time.timeScale =0f;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+
